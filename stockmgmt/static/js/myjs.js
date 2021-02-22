ff1353ac20eb72989746141b76ad242e911f5a23
Tejal Jadhav <tejadhav1996@gmail.com>
2021-02-22 16:04:49 +0530

    Initial commit

---
+++ stockmgmt/static/js/myjs.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+     $(".datetimeinput").datepicker({changeYear: true,changeMonth: true, dateFormat: 'yy-mm-dd'});
+
+     $('.table').paging({limit:5});
+
+     NProgress.start();
+     NProgress.done();
+
+
+});
